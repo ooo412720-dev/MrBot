48eda2abd152558269b2629a7dc87bebd4f96516
ooo412720-dev <ooo412720@gmail.com>
2026-06-29 20:12:58 +0300

    Update bot.py

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,14 +1,4 @@
-# app/bot/bot.py
-
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 
-from app.core.config import settings
-
-
-bot = Bot(
-    token=settings.BOT_TOKEN,
-    default=DefaultBotProperties(
-        parse_mode="HTML"
-    )
-)
+bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=None))
